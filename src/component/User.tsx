a52f47fb20f5d50c5acc934730479e568c4c894c
dcoder-x <ayomikunfaluyi@gmail.com>
2023-01-10 00:56:49 +0100

    fetch data from axios, set up user table to display data

--- src/component/User.tsx
+++ src/component/User.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { UserInfoCards } from "./Cards";
 import { userCards } from "../constants/userCards";
 import "../styles/user.scss";
@@ -6,29 +6,68 @@ import axios from "axios";
 import { ReactSVG } from "react-svg";
 import { assets } from "../assets";
 const User = () => {
-
   //tracking axios reponse state
-  const [records, setRecords] = useState <object[]>()
+  const [records, setRecords] = useState<object[]>();
+  const [filteredRecords, setFilteredRecords] = useState<object[]>();
+  const [limit, setLimit] = useState<number>(15);
+  const [low_Range_Multiplier, setLowRangeMultiplier] = useState<number>(0);
+  const [high_Range_Multiplier, setHighRangeMultiplier] = useState<number>(1);
+
+  //this converts createdAt value to date string
+  function getDateJoined(date: Date): any {
+    const formatedDate= new Date(date).toDateString()
+    return formatedDate ;
+  }
+
+  //this is the algorithm to control the number of users shown
+  useCallback(
+    function userRange() {
+      const lower_Range: number = limit * low_Range_Multiplier;
+      const higher_Range: number = limit * high_Range_Multiplier;
+
+      const filteredRecords: object[] | undefined = records?.filter(
+        (record, index) => {
+          return index >= lower_Range && index <= higher_Range;
+        }
+      );
+      setFilteredRecords(filteredRecords);
+    },
+    [limit, low_Range_Multiplier, high_Range_Multiplier]
+  );
 
   //useEffect hook
   useEffect(() => {
-
     //declare function to fetch user from api endpoint
     async function fetchUser() {
       try {
         const response = await axios.get(
           "https://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1/users"
         );
-        console.log(response.data)
-        setRecords(response.data)
-      } catch (error:any) {
-        console.error(error)
+        console.log(response.data);
+        setRecords(response.data);
+      } catch (error: any) {
+        console.error(error);
       }
     }
-    fetchUser()
-  }, []);
+    fetchUser();
+    function userRange() {
+      const lower_Range: number = limit * low_Range_Multiplier;
+      const higher_Range: number = limit * high_Range_Multiplier;
 
-//array to hold table headers
+      const filteredRecords: object[] | undefined = records?.filter(
+        (record, index) => {
+          console.log(index,lower_Range,higher_Range)
+
+          return index >= lower_Range && index <= higher_Range;
+        }
+      );
+      setFilteredRecords(filteredRecords);
+      console.log(filteredRecords)
+    }
+    userRange()
+  }, [records,limit, low_Range_Multiplier, high_Range_Multiplier]);
+
+  //array to hold table headers
   const tableHeads: string[] = [
     "organization",
     "Username",
@@ -61,17 +100,25 @@ const User = () => {
               {tableHeads.map((th, i) => {
                 return (
                   <th>
-                    {th} <ReactSVG src={assets.icons.filter} />
+                    <div>
+                      <p>{th}</p> <ReactSVG src={assets.icons.filter} />
+                    </div>
                   </th>
                 );
               })}
             </tr>
           </thead>
           <tbody>
-            {records?.map((record, i) => {
+            {filteredRecords?.map((record, i) => {
               return (
                 <>
-                  <tr></tr>
+                  <tr>
+                    <td>{record.orgName}</td>
+                    <td>{record.userName}</td>
+                    <td>{record.email}</td>
+                    <td>{record.profile.phoneNumber}</td>
+                    <td>{getDateJoined(record.createdAt)}</td>
+                  </tr>
                 </>
               );
             })}
